96eb7fb81959f9fe6912bb6fc935db20668b3edf
lautarocoello <lautarocoello@gmail.com>
2022-04-07 15:08:03 -0300

    changes for deploy

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,26 +1,63 @@
-import React from 'react'
-import './contact.css'
-import { FiMail } from 'react-icons/fi'
-
+import React from "react";
+import "./contact.css";
+// import { FiMail } from 'react-icons/fi'
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+const { YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, YOUR_USER_ID } = process.env;
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        YOUR_SERVICE_ID,
+      YOUR_TEMPLATE_ID,
+        form.current,
+        YOUR_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <section id='contact'>    
+    <section id="contact">
       <h5>Antes de irte!</h5>
       <h2>Contáctame</h2>
 
-        <div className='container contact__container'>
-          <div className="contact__options">
+      <div className="container contact__container">
+        {/* <div className="contact__options">
             <article className='contact__option'>
               <FiMail className="contact__option-icon" />
               <h4>Email</h4>
               <h5>lautarocoello@gmail.com</h5>
               <a href="mailto:lautarocoello@gmail.com">Envíame un email</a>
             </article>
-          </div>
-        </div>
-      </section>
-  )
-}
+          </div> */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Tu nombre" required />
+          <input type="email" name="email" placeholder="Tu email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Tu mensaje"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Enviar
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }><IoHome /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : '' }><GiPuzzle /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : '' }><RiServiceLine /></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }><SiAboutdotme /></a>
+      <a href="#certifications" onClick={() => setActiveNav('#certifications')} className={activeNav === '#certifications' ? 'active' : '' }><SiAboutdotme /></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : '' }><VscFolderActive /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : '' }><MdConnectWithoutContact /></a>
     </nav>
